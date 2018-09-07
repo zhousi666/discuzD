a1eeea97fcff201eeccb64ed587984b047f8aeee
StaryCatAN <494332259@qq.com>
2018-09-07 17:47:44 +0800

    域名试验
    删除yarn
    
    package.json
    
    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,49 +2,12 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
+            <a
           href="https://vuejs.org"
           target="_blank"
         >
           Core Docs
         </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
     <h2>Ecosystem</h2>
     <ul>
       <li>
@@ -88,7 +51,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: '我的网站'
     }
   }
 }
