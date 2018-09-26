842851c4607643e2882d59222f4b05b073ce07ca
StaryCatAN <494332259@qq.com>
2018-09-26 14:54:13 +0800

    TopBar

--- src/App.vue
+++ src/App.vue
@@ -3,53 +3,20 @@
     <!-- <img src="./assets/logo.png"> -->
     <el-container>
       <el-header>
-        <div class = 'global-title'>
-          <div style="display: flex; flex-direction: row;">
-          <img src="./assets/i-neurology.png" style="width: 60px; height: 60px">
-          <div style = "height: 60px;line-height: 60px;">我的个人博客</div>
-          </div>
-          <div class="menu-bar">
-            <el-menu
-            :default-active="activeIndex"
-            class="el-menu-vertical-demo"
-            mode="horizontal">
-              <el-menu-item index='1'>
-                首页
-              </el-menu-item>
-              <el-menu-item index='2'>
-                文章
-              </el-menu-item>
-            </el-menu>
-          </div>
-          <div class = 'global-login'>
-            <el-button type="text">
-              登录
-            </el-button>
-            <el-button type="text">
-              退出
-            </el-button>
-          </div>
-          </div>
+        <TopBar/>
       </el-header>
-      <el-container>
-        <el-aside>
-          <PersonalInfo/>
-        </el-aside>
-        <el-main>
-         <router-view/>
-        </el-main>
-      </el-container>
+        <router-view/>
     </el-container>
   </div>
 </template>
 
 <script>
-import PersonalInfo from './components/PersonalInfo'
+import TopBar from './components/TopBar'
 
 export default {
   name: 'App',
   components: {
-    PersonalInfo
+    TopBar
   },
   data () {
     return {
